4a6bec9e3a6ac431232b58a32f2bfdf0e7247652
alexlieu <alieu2001@googlemail.com>
2023-04-26 12:56:00 +0100

    complete task 2

--- src/main/java/example/project/Refactoring/DelegateMethods.java
+++ src/main/java/example/project/Refactoring/DelegateMethods.java
@@ -26,6 +26,7 @@ public class DelegateMethods {
      */
     public static int createRandomInteger(int max) {
         // TODO: delegate the call to `createRandomValue()` method.
-        return -1;
+        int randomValue = createRandomValue(0, max);
+        return randomValue;
     }
 }
